5025e8352dfa8df031c77c0025bc9ad7d51534ea
Alexandr Sekerin <blazzed21@gmail.com>
2025-02-07 18:28:32 +0300

    fix: change date, types and subjectDetails

--- scripts/parser.js
+++ scripts/parser.js
@@ -1,13 +1,12 @@
 import { chromium } from "playwright";
 
 export const parseSchedule = async (username, password, headless = true) => {
+  let browser;
   try {
-    const browser = await chromium.launch({ headless: headless });
-
+    browser = await chromium.launch({ headless: headless });
     const page = await browser.newPage();
 
     await page.goto("https://umeos.ru/login/index.php");
-
     await page.waitForTimeout(500);
     await page.locator('input[name="username"]').fill(username);
     await page.waitForTimeout(1000);
@@ -15,11 +14,11 @@ export const parseSchedule = async (username, password, headless = true) => {
     await page.waitForTimeout(1000);
 
     await page.click("#loginbtn", { delay: 1500 });
+
     await page.waitForURL("https://umeos.ru/my/", { waitUntil: "load" });
 
     await page.waitForTimeout(1000);
     await page.goto("https://umeos.ru/blocks/umerasp/schedule.php?t=student");
-
     await page.waitForSelector("#sched_tabs");
     await page.waitForTimeout(1000);
 
@@ -42,35 +41,37 @@ export const parseSchedule = async (username, password, headless = true) => {
 
             const dateSplit = currentDate.split(" ");
             const dayOfWeek = dateSplit[0].trim();
-            const dateOfSubject = dateSplit[1].trim();
+            const dateOfSubject = dateSplit[1].trim().split(".");
+            const day = dateOfSubject[0];
+            const month = dateOfSubject[1];
+            const year = dateOfSubject[2];
 
+            const date = { day, month, year };
             const place = cells[2].innerText.trim();
-
             const subjectCell = cells[3];
             const subjectText = subjectCell.innerHTML.trim();
-
             const subjectParts = subjectText
               .split("<br>")
               .map((part) => part.trim());
 
-            const type =
+            const types =
               Array.from(subjectCell.querySelectorAll(".badge")).map((badge) =>
                 badge.innerText.trim()
               ) || [];
 
+            const type = types.length === 1 ? types[0] : types.join(", ");
+
             const subjectDetails = {
-              subject: subjectParts[0].trim(),
-              lecturer: subjectParts[1].trim(),
+              subject: subjectParts[0]?.trim() || "",
+              lecturer: subjectParts[1]?.trim() || "",
               type: type,
-              webinarLink: subjectCell.querySelector("a")
-                ? subjectCell.querySelector("a").href
-                : "",
+              webinarLink: subjectCell.querySelector("a")?.href || "",
             };
 
             return {
               class: classNumber,
               dayOfWeek: dayOfWeek,
-              date: dateOfSubject,
+              date: date,
               startTime: startTime,
               endTime: endTime,
               place: place,
@@ -82,12 +83,13 @@ export const parseSchedule = async (username, password, headless = true) => {
         .filter((schedule) => schedule !== null);
     });
 
-  await browser.close();
-
-  return schedule;
-
+    return schedule;
   } catch (error) {
     console.error("An error occurred:", error);
     return [];
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
   }
-} 
+};
