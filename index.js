d63cdec4b4182bafb4755ce69f11b5864e6fff9c
Alexandr Sekerin <blazzed21@gmail.com>
2025-02-07 18:29:30 +0300

    feat: add calendar creation in `index.js` script

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-import 'dotenv/config';
+import "dotenv/config";
+import fs from "fs";
 
-import { parseSchedule } from './scripts/parser.js';
+import { parseSchedule } from "./scripts/parser.js";
+import { generateCalendar } from "./scripts/generatorCalendar.js";
 
-const schedule = await parseSchedule(process.env.UMTE_USERNAME, process.env.UMTE_PASSWORD);
+const schedule = await parseSchedule(
+  process.env.UMTE_USERNAME,
+  process.env.UMTE_PASSWORD
+);
 
-console.log(schedule);
+if (schedule.length) {
+  fs.writeFileSync(
+    "calendar/schedule.json",
+    JSON.stringify(schedule, null, 4),
+    "utf-8"
+  );
+  
+  const calendar = await generateCalendar(schedule);
+  fs.writeFileSync("calendar/calendar.ics", calendar.toString(), "utf-8");
+}
+
+
+
+
